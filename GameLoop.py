24f150979628491262d027695d3a342e755addbe
Vladislav <vlad_moryakov_prog@mail.ru>
2021-11-16 19:54:50 +0300

    first commit

---
+++ GameLoop.py
@@ -0,0 +1,20 @@
+import pygame, sys
+
+pygame.init()
+
+screen_width = 1200
+screen_width = 700
+
+screen = pygame.display.set_mode((screen_width, screen_width))
+clock = pygame.time.Clock()
+
+while True:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			pygame.quit()
+			sys.exit()
+
+	screen.fill("black")
+	pygame.display.update()
+	clock.tick(60)
+
